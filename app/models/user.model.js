ff964b94d20111533852e69572c28afde70f54e8
xin chen <cxin7@outlook.com>
2016-09-13 14:16:19 +0800

    auth

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,8 +1,49 @@
-var mongoose = require('mongoose');
-
-var userSchema = new mongoose.Schema({
-  email: String,
+var mongoose = require('mongoose'),
+bcrypt = require('bcrypt');
+var userSchema = mongoose.Schema({
   user_name: String,
-  password: String
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+  },
+  password: {
+    type: String,
+    required: [true,'Password is required'],
+    validate: [
+      function(password) {
+        return password.length >= 6;
+      },
+      'Password is too short'
+    ]
+  }
 });
+
+//bcrypt
+userSchema.pre('save', function(next){
+  var user = this;
+  if(user.isModified('password')){
+    bcrypt.genSalt(5,function(err,salt){
+      if(err) return next(err);
+      //hash it
+      bcrypt.hash(user.password,salt,function(err,hash){
+        //store
+        user.password = hash;
+        next();
+      });
+    });
+  }else {
+    next();
+  }
+});
+userSchema.methods.authenticate = function(password,callback){
+  //compare password
+  bcrypt.compare(password,this.password, function(err, is_match){
+    callback(null,is_match);
+  });
+};
+
 module.exports = mongoose.model('User',userSchema);
---
+++ config/passport.js
@@ -0,0 +1,85 @@
+var LocalStrategy = require('passport-local').Strategy,
+  User = require('../app/models/user.model');
+
+
+module.exports = function(passport) {
+  // passport session setup required for persistent login sessions
+  // passport needs ability to serialize and unserialize users out of session
+
+  //used to serialize the user for the session
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  //deserialize user
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+  //sign up
+  passport.use('signup', new LocalStrategy({
+      usernameField: 'email',
+      passwordField: 'password',
+      passReqToCallback: true //allows us to pass back the entire req to the callback
+    },
+    function(req, email, password, done) {
+      //asynchronous
+      process.nextTick(function() {
+        User.findOne({
+          'email': email
+        }, function(err, user) {
+          if (err) return next(err);
+          //check if there is already a user with that email
+          if (user) {
+            return done(null, false, req.flash('signupMessage', 'That email is already taken'));
+          } else {
+            var newUser = new User();
+            newUser.email = email;
+            newUser.password = password;
+            //save new user
+            newUser.save(function(err) {
+              if (err) throw err;
+              return done(null, newUser);
+            });
+          }
+        });
+      });
+    }
+  ));
+
+  //login
+  passport.use('login', new LocalStrategy({
+      usernameField: 'email',
+      passwordField: 'password',
+      passReqToCallback: true
+    },
+    function(req, email, password, done) {
+      process.nextTick(function() {
+        //see if the user trying to login already exists
+        User.findOne({
+          'email': email
+        }, function(err, user) {
+          if (err) return next(err);
+          //if user found
+          if (user) {
+            user.authenticate(password, function(err, match_password) {
+              if (match_password) {
+                return done(null, user);
+              } else {
+                return done(null, false, req.flash('loginMessage', 'Wrong password'));
+              }
+            });
+          } else {
+            //if user not found
+            return done(null, false, req.flash('loginMessage', 'No user found'));
+          }
+        });
+      });
+    }
+  ));
+
+
+
+
+};
