a113df7fe3d6467c9318f3f0e4ac126a671339e7
Kai Lin Tan <kailin.tan.2010@gmail.com>
2016-09-13 10:48:02 +0800

    Add url shorten function

--- public/js/script.js
+++ public/js/script.js
@@ -14,5 +14,28 @@ $(function(){
     $(this).children('.selected-tick').toggleClass("selected");
   });
 
+  //url shortener
+  $("#homeForm").submit(function(e){
+    e.preventDefault();
+    var api_url = "https://sjehutch-passbeemedia-shorturl.p.mashape.com/CreateUrl";
+    //user input
+    var queryUrl = $('#imgUrl').val();
+    //retrieve form object
+    var form = this;
+    //make ajax call to url shortener
+    $.ajax({
+      url: api_url,
+      dataType: "xml",
+      headers: {
+        "X-Mashape-Key": "ZZdWwwpLxlmsh4gztf1wv4yL6pLBp1Km2bVjsngmxiEKPQ39vr"
+      },
+      data: {
+        real_url: queryUrl
+      }
+    }).done(function(data){
+      $('#imgUrl').val(data.getElementsByTagName('ShortUrl')[0].innerHTML);
+      form.submit();
+    });
+  });
 
 });
