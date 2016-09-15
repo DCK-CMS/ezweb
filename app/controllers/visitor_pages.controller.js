86eeb36ba56df7db194c7b328e488863e016be41
Dylan Ng <ngxinde@gmail.com>
2016-09-15 22:29:16 +0800

    added landing page

--- app/controllers/visitor_pages.controller.js
+++ app/controllers/visitor_pages.controller.js
@@ -2,6 +2,27 @@ var Page = require('mongoose').model('Page');
 var Header = require('mongoose').model('Header');
 
 module.exports =  {
+  // Landing page (root)
+  getRoot: function(req, res, next) {
+    var pageArr = [];
+    Page.find({}, function(err, pages){
+      if(err) return next(err);
+
+      // loop through retreieve page object
+      for (var i = 0; i < pages.length; i++) {
+        var pgObj = {
+          title: pages[i].title,
+          slug: pages[i].slug
+        };
+        pageArr.push(pgObj);
+      }
+
+      res.render('landing_page', {
+        arr: pageArr
+      });
+    });
+  },
+
   // Visitor side logic
   getPage: function(req, res, next){
     var pageArr = [];
@@ -39,9 +60,6 @@ module.exports =  {
       }
 
     });
-  },
-
-  getHome: function(req, res) {
-    res.render('404');
   }
+
 };
