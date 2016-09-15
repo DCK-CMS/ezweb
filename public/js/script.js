df93a39fb2fc245776f88cc98aa80f3994e64038
xin chen <cxin7@outlook.com>
2016-09-15 13:56:12 +0800

    home page

--- public/js/script.js
+++ public/js/script.js
@@ -1,47 +1,48 @@
-$(function(){
+$(function() {
   /* ---- Toggle Sidebar ---- */
-  $("#menu-toggle").click( function(e){
+  $("#menu-toggle").click(function(e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
     $(".page-container").toggleClass("toggled");
   });
 
   /* ---- Images selection ---- */
-  var selectedImgsArr = [];
+  var ImgsArr = [];
 
   $(".img-item").click(function(e) {
     e.preventDefault();
-    var imageId;
-
     // toggling selection CSS
     $(this).toggleClass("selected");
     $(this).children('.selected-tick').toggleClass("selected");
-
-    // get the selected image id
-    imageId = $(this).children('.select-img').data('id');
-    console.log(imageId);
-    // if array does not contain imageId
-    if(selectedImgsArr.indexOf(imageId) === -1 ) {
-      selectedImgsArr.push(imageId);
-      console.log(selectedImgsArr);
-    } else {
-      selectedImgsArr.splice(selectedImgsArr.indexOf(imageId), 1);
-      console.log(selectedImgsArr);
+    var id = $(this).children('.select-img').data('image').id;
+    //push selected imgs to an array
+    if (ImgsArr.length > 0) {
+      for (var i = 0; i < ImgsArr.length; i++) {
+        if (ImgsArr[i].id === id) {
+          ImgsArr.splice(i, 1);
+          console.log(ImgsArr);
+          return;
+        }
+      }
     }
-
+    ImgsArr.push({
+      id: id,
+      url: $(this).children('.select-img').data('image').url
+    });
+    console.log(ImgsArr);
   });
   /* ---- Images selected submit ---- */
   $(".chooseImg-btn").click(function(e) {
     e.preventDefault();
-    for(var n = 0; n < selectedImgsArr.length; n++){
-      var html =
+    for (var n = 0; n < ImgsArr.length; n++) {
+      var html = "<a><img class='select-img' src='" + ImgsArr[n].url + "' /><textarea rows='3'cols='15' name='description'></textarea><input type='hidden' name='img' value='" + ImgsArr[n].id + "'></a>";
       $('.selected-img-container').prepend(html);
     }
 
   });
 
   //url shortener
-  $("#homeForm").submit(function(e){
+  $("#homeForm").submit(function(e) {
     e.preventDefault();
     var api_url = "https://sjehutch-passbeemedia-shorturl.p.mashape.com/CreateUrl";
     //user input
@@ -58,7 +59,7 @@ $(function(){
       data: {
         real_url: queryUrl
       }
-    }).done(function(data){
+    }).done(function(data) {
       $('#imgUrl').val(data.getElementsByTagName('ShortUrl')[0].innerHTML);
       form.submit();
     });
